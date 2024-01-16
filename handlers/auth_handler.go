538ec63ac7857374256e922266443785c504b0c9
JusAeng <justo_play@hotmail.com>
2024-01-17 01:23:16 +0700

    add authen routes and handler

---
+++ handlers/auth_handler.go
@@ -0,0 +1,64 @@
+package handlers
+
+import (
+	"bytes"
+	"encoding/json"
+
+	"github.com/gofiber/fiber/v2"
+
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+func GetLineProfileByTokenIdHandler(c *fiber.Ctx) error {
+	tokenId := c.Params("id")
+
+	// manga, err := repo.GetMangaById(mangaId)
+	// if err != nil {
+	// 	return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	// }
+
+	// URL of the API endpoint for the POST request
+	profilePayload := url.Values{
+		"id_token":  {tokenId},
+		"client_id": {"2002829031"},
+	}
+
+	// Create a request with the payload
+	req, err := http.NewRequest("POST", "https://api.line.me/oauth2/v2.1/verify", bytes.NewBufferString(profilePayload.Encode()))
+	if err != nil {
+		return err
+	}
+
+	// Set headers
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	// Make the request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	// Parse the response body into a map[string]interface{}
+	var responseBody map[string]interface{}
+	if err := json.Unmarshal(body, &responseBody); err != nil {
+		return err
+	}
+
+	// Check the response status
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Error: %s", string(body))
+	}
+
+	// Return the parsed response body as JSON
+	return c.JSON(responseBody)
+}
--- router/route.go
+++ router/route.go
@@ -6,9 +6,13 @@ import (
 )
 
 func Run(app *fiber.App){
+	// manga
 	app.Get("/mangas", handlers.GetMangasHandler)
 	app.Get("/mangas/:id", handlers.GetMangaByIdHandler)
 	app.Get("/mangas/title/:title", handlers.GetMangaByTitleHandler)
 	app.Delete("/mangas/:title", handlers.DeleteMangaByTitleHandler)
 	app.Post("/mangas", handlers.AddMangaHandler)
+
+	//auth
+	app.Get("/auth/:id",handlers.GetLineProfileByTokenIdHandler)
 }
