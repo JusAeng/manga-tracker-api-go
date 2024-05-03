b0fc83cf2281aafaf4e6717bb9b5f315cdf78a9c
JusAeng <justo_play@hotmail.com>
2024-05-03 10:15:56 +0700

    bug: random manga service

--- handlers/manga_handlers/manga_handler.go
+++ handlers/manga_handlers/manga_handler.go
@@ -2,7 +2,6 @@ package manga_handlers
 
 import (
 	"log"
-	"math/rand"
 	"net/url"
 
 	"github.com/JusAeng/manga-tracker-api-go/models"
@@ -59,22 +58,7 @@ func GetMangaTrending(c *fiber.Ctx) error {
 	if err != nil {
 		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
 	}
-	service.Shuffle(allManga)
-	selected := make([]*models.Manga, 3)
-	runner := 0
-    for i := 0; i < len(allManga); i++ {
-        pick := rand.Intn(2) == 0
-		if (pick){
-			selected[runner] = allManga[i]
-			runner +=1
-		}
-		if (runner >= 3 || len(selected) >= 3) {
-			break
-		}
-		if (i + 3 == len(allManga)+len(selected)){
-			break
-		}
-    }
+	selected := service.RandomManga(allManga,3)
 	return c.JSON(selected)
 }
 
@@ -83,22 +67,8 @@ func GetMangaRecommend(c *fiber.Ctx) error {
 	if err != nil {
 		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
 	}
-	service.Shuffle(allManga)
-	selected := make([]*models.Manga, 8)
-	runner := 0
-    for i := 0; i < len(allManga); i++ {
-        pick := rand.Intn(2) == 0
-		if (pick){
-			selected[runner] = allManga[i]
-			runner +=1
-		}
-		if (runner >= 8 || len(selected) >= 8) {
-			break
-		}
-		if (i + 8 == len(allManga)+len(selected)){
-			break
-		}
-    }
+	selected := service.RandomManga(allManga,8)
+
 	return c.JSON(selected)
 }
 
@@ -107,22 +77,7 @@ func GetMangaNew(c *fiber.Ctx) error {
 	if err != nil {
 		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
 	}
-	service.Shuffle(allManga)
-	selected := make([]*models.Manga, 5)
-	runner := 0
-    for i := 0; i < len(allManga); i++ {
-        pick := rand.Intn(2) == 0
-		if (pick){
-			selected[runner] = allManga[i]
-			runner +=1
-		}
-		if (runner >= 5 || len(selected) >= 5) {
-			break
-		}
-		if (i + 5 == len(allManga)+len(selected)){
-			break
-		}
-    }
+	selected := service.RandomManga(allManga,5)
 	return c.JSON(selected)
 }
 
--- service/manga_service.go
+++ service/manga_service.go
@@ -12,3 +12,20 @@ func Shuffle(arr []*models.Manga) {
         arr[i], arr[j] = arr[j], arr[i]
     }
 }
+
+func RandomManga(allManga []*models.Manga,numb int) ([]*models.Manga){
+	var selected []*models.Manga
+    for i := 0; i < len(allManga); i++ {
+        pick := rand.Intn(2) == 0
+		if (i + numb >= len(allManga)+len(selected)){
+			pick = true
+		}
+		if (pick){
+			selected = append(selected, allManga[i])
+		}
+		if (len(selected) >= numb) {
+			break
+		}
+    }
+    return selected
+}
