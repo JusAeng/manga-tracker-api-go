ae0f49ee6cef072db4103148c611cb9387aae0fa
JusAeng <justo_play@hotmail.com>
2024-05-03 10:34:26 +0700

    suffle

--- service/manga_service.go
+++ service/manga_service.go
@@ -15,6 +15,8 @@ func Shuffle(arr []*models.Manga) {
 
 func RandomManga(allManga []*models.Manga,numb int) ([]*models.Manga){
 	var selected []*models.Manga
+    Shuffle(allManga)
+
     for i := 0; i < len(allManga); i++ {
         pick := rand.Intn(2) == 0
 		if (i + numb >= len(allManga)+len(selected)){
